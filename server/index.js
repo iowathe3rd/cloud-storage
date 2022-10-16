170662ef3cedd17f67699276c5ef91ffe87cec41
iowathe3rd <beglerov04@mail.ru>
2022-10-16 20:41:44 +0600

    added signin/login by jwt toket

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const authRouter = require("./routes/auth.routes");
+const app = express();
+const PORT = config.get("serverPort");
+
+app.use(express.json());
+app.use("/api/auth", authRouter);
+
+const start = async () => {
+	try {
+		mongoose.connect(config.get("dbUrl"));
+		app.listen(PORT, () => {
+			console.log(`server started at port ${PORT}`);
+		});
+	} catch (e) {
+		throw e;
+	}
+};
+
+start();
